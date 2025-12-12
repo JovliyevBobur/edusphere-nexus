f3482cf63b9620a0b72d030021290eabe7cc949e
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2025-12-12 16:11:55 +0500

    zo'r

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection: React.FC = () => {
   const stats = [
     { icon: Users, value: '295+', label: t('students') },
     { icon: BookOpen, value: '29+', label: t('teachersCount') },
-    { icon: Calendar, value: '3+', label: t('yearsExp') },
+    { icon: Calendar, value: '4+', label: t('yearsExp') },
     { icon: Award, value: '50+', label: t('awards') },
   ];
 
